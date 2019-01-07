16b9c20a9d8489bdef7ba28574e9f353a5effb9d
Travis Decker <decker4693@gmail.com>
2019-01-06 17:16:35 -0700

    Set up Title Field in Crime Fragment.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,4 +1,48 @@
 package com.bignerdranch.android.criminalintent;
 
-public class CrimeFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+
+    private Crime crime;
+    private EditText titleField;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        crime = new Crime();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        titleField = v.findViewById(R.id.crime_title);
+        titleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //Intentionally left blank.
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                crime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //Intentionally left blank.
+            }
+        });
+
+        return v;
+    }
 }
