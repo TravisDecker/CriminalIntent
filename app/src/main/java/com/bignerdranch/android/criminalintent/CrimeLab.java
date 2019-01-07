d4abc801af17ac30d0ae7cc2a808454e1f460922
Travis Decker <decker4693@gmail.com>
2019-01-06 18:00:30 -0700

    Created CrimeLab class.

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -0,0 +1,37 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+
+    private static CrimeLab crimeLab;
+    private List<Crime> crimes;
+
+    public CrimeLab(Context context) {
+        crimes = new ArrayList<>();
+    }
+
+    public static CrimeLab get(Context context) {
+        if (crimeLab == null) {
+            crimeLab = new CrimeLab(context);
+        }
+        return crimeLab;
+    }
+
+    public List<Crime> getCrimes() {
+        return crimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : crimes) {
+            if (crime.getId() == id)
+                return crime;
+        }
+        return null;
+    }
+
+}
